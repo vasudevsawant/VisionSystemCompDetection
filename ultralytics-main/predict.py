86da65ef5a6caae4609b29349a22fb102653b676
VisionSystemCompDetection <vasusawant98@gmail.com>
2024-02-12 14:15:26 +0530

    Initial commit

--- ultralytics-main/predict.py
+++ ultralytics-main/predict.py
@@ -4,7 +4,7 @@ from ultralytics import YOLO
 model = YOLO('C:\\Users\\z004uvjn\\Desktop\\Vision\\ultralytics-main\\runs\\detect\\train\\weights\\best.pt')
 
 # Define path to the image file
-detection_output =model.predict(source='C:\\Users\\z004uvjn\\Desktop\\Vision\\ultralytics-main\\1.jpg', save=True, conf=0.2)
+detection_output =model.predict(source='C:\\Users\\z004uvjn\\Desktop\\Vision\\ultralytics-main\\1.jpg', save=True, conf=0.5)
 
 
 
